b8add37157ea19bcab4b106a6d38b54e90f02e9e
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2025-11-22 14:52:29 -0300

    Feat: Criando Usuario

---
+++ src/main/java/br/com/alunoonline/api/domain/Usuario.java
@@ -0,0 +1,25 @@
+package br.com.alunoonline.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuario")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String senha;
+
+}
